a5360982e8ac09911f74d2b85cd4ab8a11de51d2
Roshan Pawar <steradian614@gmail.com>
2020-10-31 14:37:30 -0700

    Infinite loop somewhere

--- pa/pa2/rr.cpp
+++ pa/pa2/rr.cpp
@@ -114,11 +114,11 @@ bool good(PNG &image, vector<vector<int>> &D,
   // An entry in distance table D is -1 only if the pixel has not been
   // visited.
 
-  if (next[0] < image.width() && next[0] >= 0 && next[1] < image.height() && next[1] >= 0)
+  if (unsigned(next.first) < image.width() && unsigned(next.first) >= 0 && unsigned(next.second) < image.height() && unsigned(next.second) >= 0)
   {
-    if (D[next[0], next[1]] != -1)
+    if (D[next.first][next.second] != -1)
     {
-      return closeEnough(image.getPixel(curr[0], curr[1]), image.getPixel(next[0], next[1]));
+      return closeEnough(*image.getPixel(curr.first, curr.second), *image.getPixel(next.first, next.second));
     }
   }
 
@@ -127,73 +127,115 @@ bool good(PNG &image, vector<vector<int>> &D,
 
 vector<pair<int, int>> neighbors(pair<int, int> curr)
 {
-
   vector<pair<int, int>> n;
   vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-  for (auto dir : dirs)
+  for (auto const &dir : dirs)
   {
-    pair<int, int> neighbor(curr[0] + dir[0], curr[1] + dir[1]);
+    pair<int, int> neighbor(curr.first + dir[0], curr.second + dir[1]);
     n.push_back(neighbor);
-    return n;
   }
+  return n;
 }
 
-  void rainbowRipple(PNG & image, pair<int, int> start, string sgc)
+int computeColor(int sum, vector<pair<int, RGBAPixel>> gaps)
+{
+  int curr = 0;
+  size_t gapIndex = 0;
+  bool flag = false;
+
+  while (curr < sum)
   {
-    // Color certain pixels in "image" based on their distance from "start"
-    // using the gaps-and-colors string "sgc" to specify the color pattern.
-    // The distance is determined by Breadth-First-Search which is a
-    // Level-Order Traversal of the tree of shortest paths from "start"
-    // whose edges are between good neighbors.  The neighbors of pixel
-    // (x,y) are (x-1,y), (x,y+1), (x+1,y), (x,y-1).  The good neighbors
-    // of (x,y) are those neighbors that
-    // 1. lie within the image,
-    // 2. are close to the color of pixel (x,y), and
-    // 3. have not been visited before.
-    // BFS visits "start", then the good neighbors of "start", then the
-    // good neighbors of these good neighbors, etc.  This forms a tree,
-    // level by level. The distance of a pixel from "start" is its level
-    // in this tree (or -1 if it hasn't been visited).  As soon as BFS
-    // visits a pixel it knows its distance from "start" and it can color
-    // the pixel based on the gaps-and-colors string "sgc".  (* However,
-    // it should wait to color the pixel until after its good neighbors
-    // are computed, since the original pixel color determines what
-    // neighbors are good. *)
-    // Coloring:
-    // Suppose (gap_1,color_1), (gap_2,color_2), ..., (gap_n,color_n) is the
-    // sequence of number,color pairs in "sgc".  Those pixels at distance
-    // gap_1 from "start" are colored color_1, those at distamce
-    // gap_1 + 1 + gap_2 are colored color_2, etc.  The gaps are the
-    // number of uncolored pixels between colored pixels along a shortest
-    // path.  The sequence specified by "sgc" should be viewed as
-    // repeating forever, so a pixel at distance 33 from "start" would be
-    // colored blue using the sequence (9,red),(4,blue),(3,green) since
-    // 33 = (9+1+4+1+3+1)+9+1+4.
-
-    vector<pair<int, RGBAPixel>> gaps = parseGaps(sgc);
-
-    Queue<pair<pair<int, int>, int>> queue;
-    vector<vector<int>> D = vec(image.height(), vector<int>(image.width(), 0));
-
-    q.enq(pair<pair<int, int> int>(start, 1));
-
-    while (!q.empty())
+    if (flag)
     {
-
-      pair<pair<int, int>, int> curr = q.deq();
-      D[curr[0][0], curr[0][1]] = -1;
-
-      for (auto &neighbor : neighbors(curr[0]))
+      curr++;
+      if (curr >= sum)
       {
-        if (good(image, D, curr, neighbor))
-        {
-          pair<pair<int, int>, int> child(neighbor, curr[1]++);
-          q.enq(child);
-          // color neighbor
-          // TODO
-          int colorIdx = computeColor(curr[1], gaps);
-
-        }
+        break;
       }
     }
+    else
+    {
+      curr = curr + gaps[gapIndex].first;
+      if (curr == sum)
+      {
+        return gapIndex;
+      }
+      gapIndex++;
+      if (gapIndex >= gaps.size())
+      {
+        gapIndex = 0;
+      }
+    }
+    flag = !flag;
   }
+  return -1;
+}
+
+void rainbowRipple(PNG &image, pair<int, int> start, string sgc)
+{
+  // Color certain pixels in "image" based on their distance from "start"
+  // using the gaps-and-colors string "sgc" to specify the color pattern.
+  // The distance is determined by Breadth-First-Search which is a
+  // Level-Order Traversal of the tree of shortest paths from "start"
+  // whose edges are between good neighbors.  The neighbors of pixel
+  // (x,y) are (x-1,y), (x,y+1), (x+1,y), (x,y-1).  The good neighbors
+  // of (x,y) are those neighbors that
+  // 1. lie within the image,
+  // 2. are close to the color of pixel (x,y), and
+  // 3. have not been visited before.
+  // BFS visits "start", then the good neighbors of "start", then the
+  // good neighbors of these good neighbors, etc.  This forms a tree,
+  // level by level. The distance of a pixel from "start" is its level
+  // in this tree (or -1 if it hasn't been visited).  As soon as BFS
+  // visits a pixel it knows its distance from "start" and it can color
+  // the pixel based on the gaps-and-colors string "sgc".  (* However,
+  // it should wait to color the pixel until after its good neighbors
+  // are computed, since the original pixel color determines what
+  // neighbors are good. *)
+  // Coloring:
+  // Suppose (gap_1,color_1), (gap_2,color_2), ..., (gap_n,color_n) is the
+  // sequence of number,color pairs in "sgc".  Those pixels at distance
+  // gap_1 from "start" are colored color_1, those at distamce
+  // gap_1 + 1 + gap_2 are colored color_2, etc.  The gaps are the
+  // number of uncolored pixels between colored pixels along a shortest
+  // path.  The sequence specified by "sgc" should be viewed as
+  // repeating forever, so a pixel at distance 33 from "start" would be
+  // colored blue using the sequence (9,red),(4,blue),(3,green) since
+  // 33 = (9+1+4+1+3+1)+9+1+4.
+
+  vector<pair<int, RGBAPixel>> gaps = parseGaps(sgc);
+
+  Queue<pair<pair<int, int>, int>> q;
+  vector<vector<int>> D(image.height(), vector<int>(image.width(), 0));
+
+  // Queue with each node = ((x,y), level)
+  q.enq(make_pair(start, 1));
+
+  while (!q.empty())
+  {
+
+    pair<pair<int, int>, int> curr = q.deq();
+    // mark x,y as visited in D
+    D[curr.first.first][curr.first.second] = -1;
+
+    // get all neighbors and enqueue them
+    for (auto &neighbor : neighbors(curr.first))
+    {
+      if (good(image, D, curr.first, neighbor))
+      {
+        pair<pair<int, int>, int> child(neighbor, curr.second++);
+        q.enq(child);
+      }
+    }
+
+    int colorIdx = computeColor(curr.second, gaps);
+    if (colorIdx != -1)
+    {
+      RGBAPixel *pixel = image.getPixel(curr.first.first, curr.first.second);
+      pixel->r = gaps[colorIdx].second.r;
+      pixel->g = gaps[colorIdx].second.g;
+      pixel->b = gaps[colorIdx].second.b;
+      pixel->a = gaps[colorIdx].second.a;
+    }
+  }
+}
